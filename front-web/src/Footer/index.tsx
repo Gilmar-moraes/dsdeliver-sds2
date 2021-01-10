62a660a4c54df8a7b10f8fbbdf755457cf1cb440
Gilmar Moraes <gilmar14moraes@hotmail.com>
2021-01-10 16:53:09 -0300

    Footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,12 +1,21 @@
 import './styles.css';
+import { ReactComponent as Youtube } from './Youtube.svg';
+import { ReactComponent as Instagram } from './Instagram.svg';
+import { ReactComponent as Linkendin } from './Linkedin.svg';
 
 function Footer() {
     return (
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a>
-                    
+                <a href="https://www.youtube.com/channel/UC3twHmWQwtqEO7u-gB_2f7g" target="new">
+                    <Youtube />
+                </a>
+                <a href="https://www.linkendin.com/school/devsuperior/" target="new">
+                <Linkendin />
+                </a>
+                <a href="https://www.instagram.com/devsuperior.ig/" target="new">
+                <Instagram />
                 </a>
             </div>
         </footer>
