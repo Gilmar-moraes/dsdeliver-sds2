acfadbc58b9116a4dd8742a83d0ffe2d55b0023e
Gilmar Moraes <gilmar14moraes@hotmail.com>
2021-01-10 09:41:46 -0300

    Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'; 
+import Navbar from './Navbar';
 import './App.css';
 
 function App() {
   return (
     <div>
-        <h3>Hello world</h3>
+        <Navbar/>
     </div>
   );
 }
