2df69b7c3b18762db353c43fe22a9bf98070cbbd
Gilmar Moraes <gilmar14moraes@hotmail.com>
2021-01-10 21:37:21 -0300

    Mapa

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,9 +1,24 @@
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() =>{
+        fetchProducts().then(response=> setProducts(response.data)).catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
-             <StepsHeader />     
+             <StepsHeader />   
+             <ProductsList products={products}/> 
+             <OrderLocation/> 
         </div>
     )
 }
